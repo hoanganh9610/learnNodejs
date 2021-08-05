89088417b3e2f5eeadc3894dfef13314490fe178
anh.nh <anh.nh@kyanon.digital>
2021-08-05 15:26:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//route
+app.get('/',(req, res) => res.send('Hello word!'))//arow function
+// app.get('/', function(req, res){//normal function
+//     res.send('Hello word!');
+// })
+// 127.0.0.1 - localhost
+app.listen(port, ()=> console.log(`listening at http://localhost:${port}`))
